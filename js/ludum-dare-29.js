f53259bb36ef578ffe8bbd8ce4c07d62a55f1aa0
Matt Spurlin <mattsp1290@gmail.com>
2014-04-25 22:50:39 -0500

    Add basic player

--- js/ludum-dare-29.js
+++ js/ludum-dare-29.js
@@ -1,11 +1,35 @@
+// Setup the main game object
 var game = new Phaser.Game(1024, 896, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-function preload(){
+// Setup game globals
+var player;
+var cursors;
 
+function preload(){
+	game.load.spritesheet('dude', 'assets/test-dude.png', 12, 12);
 }
 
 function create(){
+	// Start arcade physics
+	game.physics.startSystem(Phaser.Physics.ARCADE);
 
+	// Initialize the player
+	player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+	// Enable player physics
+	game.physics.arcade.enable(player);
+
+	// Physics properties
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 300;
+	player.body.collideWorldBounds = true;
+
+	// Add animations
+	player.animations.add('left', [0,], 10, true);
+	player.animations.add('right', [0,], 10, true);
+
+	// Initialize cursors
+	cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update(){
