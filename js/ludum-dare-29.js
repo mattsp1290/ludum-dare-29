d2318e2d35051c89f5b5815e8ffc5f9148536d96
Matt Spurlin <mattsp1290@gmail.com>
2014-04-25 22:32:37 -0500

    Add the primary game element

--- js/ludum-dare-29.js
+++ js/ludum-dare-29.js
@@ -1,1 +1,15 @@
+var game = new Phaser.Game(1024, 896, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
+
 alert("The File Loaded!");
