1607bf49a7c9648f12cb03f9d6b95586c903c040
Matt Spurlin <mattsp1290@gmail.com>
2014-04-25 22:58:17 -0500

    Remove The Loaded Alert

--- js/ludum-dare-29.js
+++ js/ludum-dare-29.js
@@ -33,7 +33,34 @@ function create(){
 }
 
 function update(){
+	player.body.velocity.x = 0;
+ 
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+ 
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+ 
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+ 
+        // player.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
 
 }
-
-alert("The File Loaded!");
